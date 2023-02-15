c78509a723df78a6f661ba5f6992d470275edcdd
wexsilon <wexsilon2001@protonmail.com>
2023-02-15 16:06:36 +0330

    enable parse json and urlencoded using body-parser

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
