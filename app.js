ff3c4c6a3b7e900b405618f9a60628710e2e9d5e
wexsilon <wexsilon2001@protonmail.com>
2023-02-16 13:00:40 +0330

    show hello world with ejs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.render('index', { title: "Git & Node.js" });
+    res.render('index', { title: "Git & Node.js", message: "Hello World!" });
 });
 
 app.listen(8000, () => {
